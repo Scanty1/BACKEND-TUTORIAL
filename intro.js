807f1fb0a47b5b994dc0f12cfe069769df5df59f
Dennis Gyebi <gyebidennis53@gmail.com>
2026-05-16 01:11:32 +0000

    Completed backend week 1 assignment

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Dennis";
+const age = 20;
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+console.log("My favorite programming language is " + favoriteLanguage);
